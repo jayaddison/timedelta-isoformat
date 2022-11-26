d3361d16893e6228cf36b1ce497cfef5c36b5d63
James Addison <jay@jp-hosting.net>
2022-11-26 16:54:04 +0000

    Add initial test suite structure

--- tests/test_timedelta_isoformat.py
+++ tests/test_timedelta_isoformat.py
@@ -1,10 +1,37 @@
 import unittest
 
+from timedelta_isoformat import timedelta
 
-valid_durations = []
 
-invalid_durations = []
+valid_durations = [
+    # empty duration
+    ("P0D", timedelta()),
+    ("PT0S", timedelta()),
+]
+
+invalid_durations = [
+    # incomplete strings
+    (""),
+    ("P"),
+]
 
 
 class TimedeltaISOFormat(unittest.TestCase):
-    pass
+    def test_fromisoformat_valid(self):
+        for duration_string, expected_timedelta in valid_durations:
+            with self.subTest(duration_string=duration_string):
+                parsed_timedelta = timedelta.fromisoformat(duration_string)
+                self.assertEqual(parsed_timedelta, expected_timedelta)
+
+    def test_fromisoformat_invalid(self):
+        for duration_string in invalid_durations:
+            with self.subTest(duration_string=duration_string):
+                with self.assertRaises(TypeError):
+                    timedelta.fromisoformat(duration_string)
+
+    def test_roundtrip_valid(self):
+        for _, valid_timedelta in valid_durations:
+            with self.subTest(valid_timedelta=valid_timedelta):
+                duration_string = valid_timedelta.isoformat()
+                parsed_timedelta = timedelta.fromisoformat(duration_string)
+                self.assertEqual(parsed_timedelta, valid_timedelta)
