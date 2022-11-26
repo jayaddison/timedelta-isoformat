193369bef26b4fd7d4d63b50c660b2286de1a602
James Addison <jay@jp-hosting.net>
2022-11-26 16:55:23 +0000

    Add naive timedelta.isoformat implementation
    This does not yet support timedeltas with microsecond durations

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -7,4 +7,21 @@ class timedelta(datetime.timedelta):
         pass
 
     def isoformat(self):
-        pass
+        if not self:
+            return "P0D"
+
+        years, months, days = 0, 0, self.days
+        hours, minutes, seconds = 0, 0, self.seconds
+        minutes, seconds = int(seconds / 60), seconds % 60
+        hours, minutes = int(minutes / 60), minutes % 60
+
+        result = "P"
+        result += f"{years}Y" if years else ""
+        result += f"{months}M" if months else ""
+        result += f"{days}D" if days else ""
+        if hours or minutes or seconds:
+            result += "T"
+            result += f"{hours}H" if hours else ""
+            result += f"{minutes}M" if minutes else ""
+            result += f"{seconds}S" if seconds else ""
+        return result
