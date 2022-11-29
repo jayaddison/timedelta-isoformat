d49395b5c6b1360951041b4a95e76ffb8694149b
James Addison <jay@jp-hosting.net>
2022-11-29 18:19:49 +0000

    Nitpick: move vertically-elongated error-check condition below other grouped lines, so that it is followed by an empty line, providing a visual code boundary

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -135,6 +135,8 @@ class timedelta(datetime.timedelta):
 
         if not measurements:
             raise _parse_error("no measurements found")
+        if "weeks" in measurements and len(measurements) > 1:
+            raise _parse_error("cannot mix weeks with other units")
         if (
             stream is time
             and "hours" not in measurements
@@ -142,8 +144,6 @@ class timedelta(datetime.timedelta):
             and "seconds" not in measurements
         ):
             raise _parse_error("no measurements found in time segment")
-        if "weeks" in measurements and len(measurements) > 1:
-            raise _parse_error("cannot mix weeks with other units")
 
         return cls(**{k: v for k, v in measurements.items() if v})
 
