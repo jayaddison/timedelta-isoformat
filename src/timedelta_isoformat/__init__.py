3e0c7d1ad57f331ef0b07d8201fe12904efe1a0a
James Addison <jay@jp-hosting.net>
2022-11-29 15:00:17 +0000

    Simplification: remove year-and-month support exception handling check and test coverage -- it seems unnecessarily complex in relation to the error messaging value it provides

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -137,13 +137,7 @@ class timedelta(datetime.timedelta):
             raise _parse_error("no measurements found in time segment")
         if "weeks" in measurements and len(measurements) > 1:
             raise _parse_error("cannot mix weeks with other units")
-
-        try:
-            return cls(**{k: v for k, v in measurements.items() if v})
-        except TypeError as exc:
-            if measurements.get("years") or measurements.get("months"):
-                raise _parse_error("year and month fields are not supported") from exc
-            raise exc
+        return cls(**{k: v for k, v in measurements.items() if v})
 
     def isoformat(self):
         """Produce an ISO8601-style representation of this :py:class:`timedelta`"""
