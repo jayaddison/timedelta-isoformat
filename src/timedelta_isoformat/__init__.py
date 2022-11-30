jayaddison/timedelta-isoformat

commit a19fefa57559f0c75d0feee1de055ece8592d013

Refactor: migrate from a boolean 'decimal' (value-contains-potential-decimal-content) to integer 'decimal_place' (string-index-of-first-potential-decimal-place)

src/timedelta_isoformat/__init__.py
@@ -97,7 +97,7 @@ class timedelta(datetime.timedelta):
         time_tokens = iter(("H", "hours", "M", "minutes", "S", "seconds"))
         week_tokens = iter(("W", "weeks"))
 
-        tokens, value, tail, decimal, measurements = None, "", None, False, {}
+        tokens, value, tail, decimal_place, measurements = None, "", 0, False, {}
         for char in duration:
             if char in _FIELD_CHARACTERS:
                 value += char
@@ -115,9 +115,9 @@ class timedelta(datetime.timedelta):
                 tokens = week_tokens
                 pass
 
-            if char in ",." and decimal is False:
+            if char in ",." and not decimal_place:
                 value += "."
-                decimal = True
+                decimal_place = len(value) - 1
                 continue
 
             # Note: this advances and may exhaust the token iterator
@@ -128,10 +128,10 @@ class timedelta(datetime.timedelta):
             assert value[0].isdigit(), f"value '{value}' does not start with a digit"
 
             try:
-                measurements[next(tokens)] = (float if decimal else int)(value)
+                measurements[next(tokens)] = (float if decimal_place else int)(value)
             except ValueError as exc:
                 raise ValueError(f"unable to parse '{value}' as a number") from exc
-            value, decimal = "", False
+            value, decimal_place = "", 0
 
         date_tail, time_tail = (tail, value) if tokens is time_tokens else (value, None)
         if date_tail:

commit c13a32acfc7cb317f424767bc5b709a63a9c8701

Fixup: 'tail' variable (expected type: str) was being initialized with the value zero (type: int)
This bug was introduced in a19fefa57559f0c75d0feee1de055ece8592d013 (unreleased)

src/timedelta_isoformat/__init__.py
@@ -104,7 +104,7 @@ class timedelta(datetime.timedelta):
         time_tokens = iter(("H", "hours", "M", "minutes", "S", "seconds"))
         week_tokens = iter(("W", "weeks"))
 
-        tokens, value, tail, decimal_mark, measurements = None, "", 0, None, {}
+        tokens, value, tail, decimal_mark, measurements = None, "", None, None, {}
         for char in duration:
             if char in _FIELD_CHARACTERS:
                 value += char