d3dd2e5c34db5ece1abde967c23cada43740a3e5
James Addison <jay@jp-hosting.net>
2022-12-01 12:21:00 +0000

    Update test expectations
    (cherry picked from commit 6f95c2c4a435eea958ec9335be91453447b93eec)

--- tests/test_timedelta_isoformat.py
+++ tests/test_timedelta_isoformat.py
@@ -81,7 +81,7 @@ invalid_durations = [
     ("P0000-1-0", "unable to parse '0000-1-0' into date components"),
     ("PT1:2:3", "unable to parse '1:2:3' into time components"),
     ("PT01:0203", "unable to parse '01:0203' into time components"),
-    ("PT01", "unable to parse '01' into time components"),
+    ("PT01", "expected a positive integer minutes component"),
     # decimals must have a non-empty integer value before the separator
     ("PT.5S", "value '.5' does not start with a digit"),
     ("P1M.1D", "value '.1' does not start with a digit"),
