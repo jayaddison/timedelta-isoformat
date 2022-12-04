40add914caba046951d48ed3c744630b2900afd7
James Addison <jay@jp-hosting.net>
2022-12-04 00:05:01 +0000

    Cleanup: make the 'inclusive_range' parameter into a non-optional boolean argument
    (cherry picked from commit 8acd759e079cd9e4fbf1da3e2f1f07ffee0870e7)

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -8,7 +8,7 @@ class timedelta(datetime.timedelta):
     """
 
     @staticmethod
-    def _filter(components, inclusive_range=None):
+    def _filter(components, inclusive_range=True):
         for quantity, unit, limit in components:
             if limit and (quantity > limit if inclusive_range else quantity >= limit):
                 bounds = f"[0..{limit}" + ("]" if inclusive_range else ")")
