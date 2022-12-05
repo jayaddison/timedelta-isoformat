306ec25db3313c1841d2790a0534a0bc1069a96e
James Addison <jay@jp-hosting.net>
2022-12-05 02:39:14 +0000

    For each pair of adjacent-unit time-related fields where unambiguous, non-lossy unit conversion is possible between them (for example, days <-> hours, minutes <-> seconds), check whether both fields contains values -- and if they are both greater than zero, then allow the value from the larger to flow into the smaller unit; this produces a more concise output representation
    Conflicts:
     src/timedelta_isoformat/__init__.py

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -175,12 +175,23 @@ class timedelta(datetime.timedelta):
         if self.days % 7 == 0 and self.seconds == 0 and self.microseconds == 0:
             return f"P{int(self.days / 7)}W"
 
+        days = self.days
         minutes, seconds = divmod(self.seconds, 60)
         hours, minutes = divmod(minutes, 60)
         if self.microseconds:
             seconds += self.microseconds / 1_000_000  # type: ignore
 
-        result = f"P{self.days}D" if self.days else "P"
+        if hours and days:
+            hours += days * 24
+            days %= 1
+        if minutes and hours:
+            minutes += hours * 60
+            hours %= 1
+        if seconds and minutes:
+            seconds += minutes * 60
+            minutes %= 1
+
+        result = f"P{days}D" if days else "P"
         if hours or minutes or seconds:
             result += "T"
             result += f"{hours}H" if hours else ""
