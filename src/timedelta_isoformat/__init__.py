4ece198b281d7e87aca4398c776f407e46b608f9
James Addison <jay@jp-hosting.net>
2023-03-13 16:41:18 +0000

    Cleanup: consolidate redundant usage of paired 'context' and 'remaining_tokens' variables

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -1,6 +1,7 @@
 """Supplemental ISO8601 duration format support for :py:class:`datetime.timedelta`"""
 import datetime
 from enum import StrEnum
+from itertools import chain
 from typing import Iterable, Tuple, TypeAlias, Dict
 from dataclasses import dataclass
 
@@ -94,22 +95,26 @@ class timedelta(datetime.timedelta):
         in order of largest-to-smallest unit from left-to-right (with the exception of
         week measurements, which must be the only measurement in the string if present).
         """
-        contexts_encountered, context, remaining_tokens, value = set(), DateContext, iter(DateContext), ""
+        date_context = iter(DateContext)
+        time_context = iter(TimeContext)
+        week_context = iter(WeekContext)
+
+        contexts_encountered, context, value = set(), date_context, ""
         for char in duration:
             if char in _DECIMAL_CHARACTERS:
                 value += char
                 continue
 
-            if char == "T" and context is not TimeContext:
-                context, remaining_tokens, value = TimeContext, iter(TimeContext), ""
+            if char == "T" and context is not time_context:
+                context, value = time_context, ""
                 continue
 
-            if char == "W" and context is DateContext:
-                context, remaining_tokens = WeekContext, iter(WeekContext)
+            if char == "W" and context is date_context:
+                context = week_context
                 pass
 
             try:
-                while (unit := next(remaining_tokens)) != char: continue
+                while (unit := next(context)) != char: continue
             except StopIteration:
                 raise ValueError(f"unexpected character '{char}'")
 
@@ -118,7 +123,7 @@ class timedelta(datetime.timedelta):
             value = ""
 
         assert contexts_encountered, "no measurements found"
-        assert WeekContext not in contexts_encountered or len(contexts_encountered) == 1, "cannot mix weeks with other units"
+        assert week_context not in contexts_encountered or len(contexts_encountered) == 1, "cannot mix weeks with other units"
 
     @classmethod
     def _from_duration(cls, duration: str) -> Measurements:
