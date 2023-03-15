1fd1fa86675bc11d1692a320124cdc7b4c3332b4
James Addison <jay@jp-hosting.net>
2023-03-15 16:39:01 +0000

    Nitpick: given two lines containing variable assignments, one of which is a multi-value assignment line, consolidate the two lines

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -102,8 +102,7 @@ class timedelta(datetime.timedelta):
         time_context = iter(TimeUnit)
         week_context = iter(WeekUnit)
 
-        context, value = date_context, ""
-        values_found = 0
+        context, value, values_found = date_context, "", 0
         for char in duration:
             if char in _DECIMAL_CHARACTERS:
                 value += char
