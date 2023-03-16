2db3f9021be227551c716a8491b8116852212d43
James Addison <jay@jp-hosting.net>
2023-03-16 15:57:22 +0000

    Revert "Cleanup: remove the redundant zero-lower-bound comparison conditions from the component bounds-checking logic"
    This reverts commit 240b4289b3560185ad8dfa6566777ebe662553bb.

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -155,7 +155,7 @@ class timedelta(datetime.timedelta):
                 raise ValueError(msg) from exc
             if not quantity:
                 continue
-            if limit and (quantity > limit if inclusive_limit else quantity >= limit):
+            if limit and not (0 <= quantity <= limit if inclusive_limit else 0 <= quantity < limit):
                 bounds = f"[0..{limit}" + ("]" if inclusive_limit else ")")
                 raise ValueError(f"{unit} value of {value} exceeds range {bounds}")
             yield unit, quantity
