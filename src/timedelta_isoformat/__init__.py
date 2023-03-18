07e46dde3870d4d4af57bf0dd226f80cafe9f3e5
James Addison <jay@jp-hosting.net>
2023-03-18 14:40:46 +0000

    Revert "Revert "Refactor: include the limit value itself (which may be None) in the component bounds-check pattern matching""
    This reverts commit a7d41bbb966c58e344f4650dba600bb5257dacd0.

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -28,13 +28,13 @@ class timedelta(datetime.timedelta):
                 raise ValueError(msg) from exc
 
         def _bounds_check(self) -> bool:
-            upper_limit_inclusive = self.limit not in (24, 60) if self.limit else None
-            match upper_limit_inclusive:
-                case None if 0 <= self.quantity: return True
-                case True if 0 <= self.quantity <= self.limit: return True
-                case False if 0 <= self.quantity < self.limit: return True
+            inclusive_limit = self.limit not in (24, 60)
+            match self.limit, inclusive_limit:
+                case None, _ if 0 <= self.quantity: return True
+                case _, True if 0 <= self.quantity <= self.limit: return True
+                case _, False if 0 <= self.quantity < self.limit: return True
 
-            bounds = f"[0..{self.limit}" + ("]" if upper_limit_inclusive else ")")
+            bounds = f"[0..{self.limit}" + ("]" if inclusive_limit else ")")
             raise ValueError(f"{self.unit} value of {self.value} exceeds range {bounds}")
 
     Components: TypeAlias = Iterable[Component]
