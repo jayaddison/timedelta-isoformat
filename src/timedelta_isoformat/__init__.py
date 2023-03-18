8118e5112368532e64ee079fbdab84a6b44b928b
James Addison <jay@jp-hosting.net>
2023-03-18 14:40:46 +0000

    Revert "Revert "Brevity / refactor: use pattern matching to classify upper-bound range check conditions""
    This reverts commit fe060359e4fbf8d4d92fb67e544c89564e56f50c.

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -28,18 +28,13 @@ class timedelta(datetime.timedelta):
                 raise ValueError(msg) from exc
 
         def _bounds_check(self) -> bool:
-            if not self.limit:
-                return True
+            upper_limit_inclusive = self.limit not in (24, 60) if self.limit else None
+            match upper_limit_inclusive:
+                case None if 0 <= self.quantity: return True
+                case True if 0 <= self.quantity <= self.limit: return True
+                case False if 0 <= self.quantity < self.limit: return True
 
-            inclusive_limit = self.limit not in (24, 60)
-            if inclusive_limit:
-                if 0 <= self.quantity <= self.limit:
-                    return True
-            else:
-                if 0 <= self.quantity < self.limit:
-                    return True
-
-            bounds = f"[0..{self.limit}" + ("]" if inclusive_limit else ")")
+            bounds = f"[0..{self.limit}" + ("]" if upper_limit_inclusive else ")")
             raise ValueError(f"{self.unit} value of {self.value} exceeds range {bounds}")
 
     Components: TypeAlias = Iterable[Component]
