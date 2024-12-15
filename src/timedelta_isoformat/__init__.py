da9c71126d088bcab93a25258a2a64894555c0fc
James Addison <jay@jp-hosting.net>
2024-12-15 00:41:26 +0000

    Refactor / performance: factor-out an attribute access

--- src/timedelta_isoformat/__init__.py
+++ src/timedelta_isoformat/__init__.py
@@ -88,7 +88,7 @@ class timedelta(datetime.timedelta):
         value, unit = "", None
         for char in duration:
             if char in {",", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}:
-                value += char if char.isdigit() else "."
+                value += "." if char == "," else char
                 continue
 
             if char == "T" and char in context:
